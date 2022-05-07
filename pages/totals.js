0d55f78fe27168e7edc09bcb5c8c311b7c11b102
Naman <naman.grover@bonamisoftware.com>
2022-05-07 18:48:09 +0530

    Added Key

--- pages/totals.js
+++ pages/totals.js
@@ -14,7 +14,7 @@ const Totals = () => {
             <h1 className='text-center text-2xl m-2 md:m-4'>Grand Total: <span>{data?.reduce((a, c) => a + c.amount, 0)}</span></h1>
             <div className='flex flex-col p-8 w-full items-center'>
                 {cats?.map((cat) => (
-                    <button onClick={() => router.push(`/spends/${cat.toLowerCase()}`)} className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-64'>{cat}</button>
+                    <button key={cat} onClick={() => router.push(`/spends/${cat.toLowerCase()}`)} className='text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800 w-64'>{cat}</button>
                 ))}
             </div>
         </div>
